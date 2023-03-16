4710d9912bf444567306a8713234386925b5669e
DaSeul.Lee <0909daseul@gmail.com>
2023-03-16 23:57:45 +0900

    style: 주석 작성

--- java/src/programmers/Lv0_숨어있는숫자의덧셈(2).java
+++ java/src/programmers/Lv0_숨어있는숫자의덧셈(2).java
@@ -5,9 +5,9 @@ class Solution {
         
         String tmp = "";
         for(char c : my_string.toCharArray()) {
-            System.out.println("c: " + c);
+            // 문자가 나올 때까지 숫자 연결
             if('0' <= c && c <= '9') tmp += c;
-            else if(tmp.length() != 0) {
+            else if(tmp.length() != 0) { // 문자가 나오면 숫자 더하기
                 answer += Integer.parseInt(tmp);
                 tmp = "";
             }
@@ -23,6 +23,7 @@ class Solution {
 // answer 2
 class Solution {
     public int solution(String my_string) {
+        // 숫자가 아니면 " " 공백으로 변환 후 공백 기준으로 split
         String[] tmp = my_string.replaceAll("[^0-9]", " ").split(" ");
 
         int answer = 0;
