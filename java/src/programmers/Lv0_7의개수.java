1c8faf6025c62b7748b75aafa9c02b41bf7e58df
DaSeul.Lee <0909daseul@gmail.com>
2023-03-22 23:54:34 +0900

    Style: 주석 추가

--- java/src/programmers/Lv0_7의개수.java
+++ java/src/programmers/Lv0_7의개수.java
@@ -1,8 +1,10 @@
 class Solution {
     public int solution(int[] array) {
+        // int 배열을 하나의 문자열로 변환
         String s = "";
         for(int i : array) s += i;
         
+        // 7 외의 숫자를 ""로 변환
         s = s.replaceAll("[^7]", "");
         
         int answer = s.length();
