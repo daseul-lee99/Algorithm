3354c871b3bfdcd9093728f90ee4acf0f9bfcc7b
DaSeul.Lee <0909daseul@gmail.com>
2023-04-04 23:57:01 +0900

    style: Add annotation

--- java/src/programmers/Lv0_잘라서배열로저장하기.java
+++ java/src/programmers/Lv0_잘라서배열로저장하기.java
@@ -1,14 +1,14 @@
 class Solution {
     public String[] solution(String my_str, int n) {
         int len = my_str.length();
-        int size = (len + n - 1) / n;
+        int size = (len + n - 1) / n; // n 길이 미만의 문자열이 나올 수 있으니까 + (n - 1)
         String[] answer = new String[size];
         
         for(int i = 0; i < size; i++) {
             int st = i * n;
             int ed = (st + n) > len ? len : (st + n);
             
-            answer[i] = my_str.substring(st, ed);
+            answer[i] = my_str.substring(st, ed); // st번째 문자 포함, ed번째 문자 미포함 [st, ed)
         }
         
         return answer;
